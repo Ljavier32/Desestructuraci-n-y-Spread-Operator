fa6b5e400226eaf142fb45d8c6aeca5c00eba3a5
Ljavier32 <lennyjavier4@gmail.com>
2025-02-25 22:18:28 -0400

    Nuevo Desestructurador
    Este es el corregido y arregaldo, mejoramos en codigo para mejorarla funcion

--- desestructuracion.js
+++ desestructuracion.js
@@ -2,16 +2,19 @@ const jugador = {
     nombre: "Steve",
     experiencia: 15,
     inventario: {
-      herramienta: "Pico de diamante",
-      comida: "Manzana dorada",
-      bloques: 64
+        herramienta: "Pico de diamante",
+        comida: "Manzana dorada",
+        bloques: 64
     }
-  };
-  
-  const { nombre, inventario: { herramienta } } = jugador;
-  console.log(`Nombre: ${nombre}`);
-  console.log(`Herramienta principal: ${herramienta}`);
-  
-  const nuevoObjeto = "Antorcha";
-  const nuevoInventario = { ...jugador.inventario, Objeto: nuevoObjeto };
-  console.log(nuevoInventario);
+};
+
+const { nombre, inventario: { herramienta } } = jugador;
+
+console.log(`Nombre del jugador: ${nombre}, su herramienta primaria es ${herramienta}`);
+
+
+const nuevoObjeto = {herramienta_adicional : "Sable"};
+
+const nuevoInventario = { ...jugador.inventario, ...nuevoObjeto };
+
+console.log("Nuevo inventario:", nuevoInventario);
